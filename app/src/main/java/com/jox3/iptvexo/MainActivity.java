86d1f1a65f843eeb542cf0fb7e57339182468499
JOX36 <angelvalenciamurcielago@gmail.com>
2026-03-30 19:53:23 -0500

    Add files via upload

--- app/src/main/java/com/jox3/iptvexo/MainActivity.java
+++ app/src/main/java/com/jox3/iptvexo/MainActivity.java
@@ -14,9 +14,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    // Lista de canales actuales para navegación en PlayerActivity
-    private String currentChannelsJson = "[]";
-    private int currentChannelIndex = -1;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -38,64 +35,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        webView.addJavascriptInterface(new CatalogBridge(), "AndroidPlayer");
+        webView.addJavascriptInterface(new Bridge(), "AndroidPlayer");
         webView.loadUrl("file:///android_asset/player.html");
     }
 
-    class CatalogBridge {
-
-        @JavascriptInterface
-        public void playUrl(String url) {}
-
+    class Bridge {
         @JavascriptInterface
         public void openPlayer(String url, String name, String group, String type,
                                String logo, String itemId, String channelsJson, int channelIndex) {
-            currentChannelsJson = channelsJson != null ? channelsJson : "[]";
-            currentChannelIndex = channelIndex;
-
-            Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
-            // FLAG_ACTIVITY_SINGLE_TOP evita apilar múltiples PlayerActivity
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.putExtra("url", url);
-            intent.putExtra("name", name);
-            intent.putExtra("group", group);
-            intent.putExtra("type", type);
-            intent.putExtra("logo", logo);
-            intent.putExtra("id", itemId);
-            intent.putExtra("channel_index", channelIndex);
-            intent.putExtra("channels_json", currentChannelsJson);
-            startActivityForResult(intent, 1001);
+            Intent i = new Intent(MainActivity.this, PlayerActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            i.putExtra("url", url);
+            i.putExtra("name", name);
+            i.putExtra("group", group);
+            i.putExtra("type", type);
+            i.putExtra("logo", logo);
+            i.putExtra("id", itemId);
+            i.putExtra("channel_index", channelIndex);
+            i.putExtra("channels_json", channelsJson != null ? channelsJson : "[]");
+            startActivityForResult(i, 1001);
         }
 
-        // Compatibilidad con llamadas sin lista de canales
-        @JavascriptInterface
-        public void openPlayerSimple(String url, String name, String group, String type, String logo, String itemId) {
-            openPlayer(url, name, group, type, logo, itemId, "[]", -1);
-        }
-
-        @JavascriptInterface
-        public void stop() {}
-
-        @JavascriptInterface
-        public void goFullscreen() {}
+        @JavascriptInterface public void playUrl(String url) {}
+        @JavascriptInterface public void stop() {}
+        @JavascriptInterface public void goFullscreen() {}
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1001 && data != null) {
-            boolean favAdded   = data.getBooleanExtra("fav_added", false);
-            boolean favRemoved = data.getBooleanExtra("fav_removed", false);
-            String itemId      = data.getStringExtra("item_id");
-            String itemType    = data.getStringExtra("item_type");
-            if (favAdded && itemId != null) {
-                webView.evaluateJavascript(
-                    "S.favs.add('" + itemType + "_" + itemId + "');saveFavs();renderItems(S.channels,'')", null
-                );
-            } else if (favRemoved && itemId != null) {
-                webView.evaluateJavascript(
-                    "S.favs.delete('" + itemType + "_" + itemId + "');saveFavs();renderItems(S.channels,'')", null
-                );
+    protected void onActivityResult(int req, int res, Intent data) {
+        super.onActivityResult(req, res, data);
+        if (req == 1001 && data != null) {
+            boolean added   = data.getBooleanExtra("fav_added", false);
+            boolean removed = data.getBooleanExtra("fav_removed", false);
+            String id   = data.getStringExtra("item_id");
+            String type = data.getStringExtra("item_type");
+            if (id != null) {
+                String key = type + "_" + id;
+                if (added)   webView.evaluateJavascript("S.favs.add('"   + key + "');saveFavs();", null);
+                if (removed) webView.evaluateJavascript("S.favs.delete('"+ key + "');saveFavs();", null);
             }
         }
     }
